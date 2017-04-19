e4d40540b59ff587f1d56f1bec86ed5d3b89844a
Daniel (Fourdee) <daniel_haze@hotmail.com>
2017-04-19 14:36:49 +0100

    v149
    + OPi Win: https://github.com/Fourdee/DietPi/issues/875
    
    + AmiBerry revet to xinit:
    https://github.com/Fourdee/DietPi/issues/850#issuecomment-295235197

--- PREP_SYSTEM_FOR_DIETPI.sh
+++ PREP_SYSTEM_FOR_DIETPI.sh
@@ -123,7 +123,7 @@ apt-get dist-upgrade -y
 
 #install packages
 echo -e "CONF_SWAPSIZE=0" > /etc/dphys-swapfile
-apt-get install -y apt-transport-https ethtool p7zip-full hfsplus iw debconf-utils xz-utils fbset wpasupplicant resolvconf bc dbus bzip2 psmisc bash-completion cron whiptail sudo ntp ntfs-3g dosfstools parted hdparm pciutils usbutils zip htop wput wget fake-hwclock dphys-swapfile curl unzip ca-certificates console-setup console-data console-common keyboard-configuration wireless-tools wireless-regdb crda --no-install-recommends
+apt-get install -y ca-certificates keyboard-configuration locales apt-transport-https ethtool p7zip-full hfsplus iw debconf-utils xz-utils fbset wpasupplicant resolvconf bc dbus bzip2 psmisc bash-completion cron whiptail sudo ntp ntfs-3g dosfstools parted hdparm pciutils usbutils zip htop wput wget fake-hwclock dphys-swapfile curl unzip ca-certificates console-setup console-data console-common keyboard-configuration wireless-tools wireless-regdb crda --no-install-recommends
 
 #??? NanoPi Neo Air 3.x kernel only (and possibily other ap62xx chipsets), required for ap6212 bt firmware service: https://github.com/Fourdee/DietPi/issues/602#issuecomment-262806470
 apt-get install rfkill
@@ -489,7 +489,6 @@ systemctl disable systemd-timesyncd
 sed -i '/verbosity=/c\verbosity=7' /boot/armbianEnv.txt
 
 
-
 dpkg-reconfigure tzdata #Europe > London
 dpkg-reconfigure keyboard-configuration #Keyboard must be plugged in for this to work!
 dpkg-reconfigure locales # en_GB.UTF8 as default and only installed locale
@@ -499,6 +498,8 @@ cat << _EOF_ > /etc/environment
 LC_ALL=en_GB.UTF-8
 LANG=en_GB.UTF-8
 _EOF_
+
+
 
 #??? ARMbian OPi Zero 2: https://github.com/Fourdee/DietPi/issues/876#issuecomment-294350580
 echo -e "blacklist bmp085" > /etc/modprobe.d/bmp085.conf
