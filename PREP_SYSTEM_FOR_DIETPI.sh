5659a1549aabb47a4d78a0441d2baafe5eb0ca16
Daniel (Fourdee) <daniel_haze@hotmail.com>
2018-03-18 20:09:38 +0000

    v6.5
    + DietPi-Config | Removed 'firmware-ralink' pre-req from WiFi enable.
    This is a virtual package for 'firmware-misc-nonfree':
    https://github.com/Fourdee/DietPi/issues/1631

--- PREP_SYSTEM_FOR_DIETPI.sh
+++ PREP_SYSTEM_FOR_DIETPI.sh
@@ -719,7 +719,7 @@ _EOF_
 		aPACKAGES_REQUIRED_INSTALL+=('crda')			# WiFi related
 		aPACKAGES_REQUIRED_INSTALL+=('firmware-atheros')	# WiFi dongle firmware
 		aPACKAGES_REQUIRED_INSTALL+=('firmware-brcm80211')	# WiFi dongle firmware
-		aPACKAGES_REQUIRED_INSTALL+=('firmware-ralink')		# WiFi dongle firmware
+		#aPACKAGES_REQUIRED_INSTALL+=('firmware-ralink')		# WiFi dongle firmware | virtual package for firmware-misc-nonfree
 		aPACKAGES_REQUIRED_INSTALL+=('iw')			# WiFi related
 		aPACKAGES_REQUIRED_INSTALL+=('rfkill')	 		# WiFi related: Used by some onboard WiFi chipsets
 		aPACKAGES_REQUIRED_INSTALL+=('wireless-tools')		# WiFi related
