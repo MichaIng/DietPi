8de02b55859a953c854f650d0c536075f59b0ba6
MichaIng <28480705+MichaIng@users.noreply.github.com>
2018-12-20 21:06:17 +0100

    v6.20
    + DietPi-WiFi-Monitor | Since this systemd unit prints to syslog/journald, reduce output to connection loss + reconnect only. Print connection checks only in debug mode.

--- rootfs/var/lib/dietpi/services/dietpi-wifi-monitor.sh
+++ rootfs/var/lib/dietpi/services/dietpi-wifi-monitor.sh
@@ -16,20 +16,22 @@
 	#-------------------------------------------------------------------------------------
 	#Main
 	#-------------------------------------------------------------------------------------
+	echo "Checking connnection for: $ADAPTER via ping to default gateway every $TICKRATE seconds"
+
 	while :
 	do
 
 		# - Get current gateway for ping
 		URL_PING="$(ip r s 0.0.0.0/0 dev $ADAPTER | awk '{print $3}')"
 
-		echo "Checking connnection for: $ADAPTER via ping to $URL_PING"
+		[[ $G_DEBUG == 1 ]] && echo "Checking connnection for: $ADAPTER via ping to $URL_PING"
 		if ping -I $ADAPTER -c 1 $URL_PING; then
 
-			echo "Connection valid for: $ADAPTER"
+			[[ $G_DEBUG == 1 ]] && echo "Connection valid for: $ADAPTER"
 
 		else
 
-			echo "Detected connection loss: $ADAPTER. Reconnecting"
+			echo "Detected connection loss: $ADAPTER. Reconnecting..."
 			ifdown "$ADAPTER"
 			sleep 1
 			ifup "$ADAPTER"
