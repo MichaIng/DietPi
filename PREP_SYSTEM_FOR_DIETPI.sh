55d14c16b801ec9e8eb382f9c56fa52477898104
Daniel Knight <daniel.knight@dietpi.com>
2018-12-22 11:42:55 +0000

    v6.20
    + Generate GPL license in PREP as file.

--- PREP_SYSTEM_FOR_DIETPI.sh
+++ PREP_SYSTEM_FOR_DIETPI.sh
@@ -1513,6 +1513,22 @@ $IMAGE_CREATOR
 $PREIMAGE_INFO
 _EOF_
 
+		G_DIETPI-NOTIFY 2 'Generating GPL license readme'
+
+		cat << _EOF_ > /var/lib/dietpi/license.txt
+-----------------------
+DietPi - GPLv2 License:
+-----------------------
+ - Use arrow keys to scrolll
+ - Press 'TAB' then 'ENTER' to continue
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 2 of the License, or any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program. If not, please see http://www.gnu.org/licenses/'
+_EOF_
+
 		G_DIETPI-NOTIFY 2 'Clearing APT cache'
 
 		G_RUN_CMD apt-get clean
