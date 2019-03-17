509f95f30e71f805d4081e5ee9afab89471191ed
Daniel Knight <daniel.knight@dietpi.com>
2019-03-17 15:05:59 +0000

    v6.22
    + Improve code to check for forwarding/proxy etc.

--- .meta/myip.php
+++ .meta/myip.php
@@ -1,1 +1,18 @@
-<?php echo $_SERVER["REMOTE_ADDR"]; ?>
+<?php
+
+$ip = isset($_SERVER['HTTP_CLIENT_IP']) ? $_SERVER['HTTP_CLIENT_IP'] : isset($_SERVER['HTTP_X_FORWARDED_FOR']) ? $_SERVER['HTTP_X_FORWARDED_FOR'] : $_SERVER['REMOTE_ADDR'];
+
+if ($ip)
+{
+
+    echo $ip;
+
+}
+else
+{
+
+	echo 'Obtain IP failed';
+
+}
+
+?>
