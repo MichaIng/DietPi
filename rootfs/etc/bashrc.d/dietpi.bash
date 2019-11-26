11197034d4064eb746d9485c6e4405565592b7c8
MichaIng <micha@dietpi.com>
2019-11-26 13:00:37 +0100

    v6.27
    + DietPi-Login | Minor coding

--- rootfs/etc/bashrc.d/dietpi.bash
+++ rootfs/etc/bashrc.d/dietpi.bash
@@ -15,7 +15,7 @@
 	#////////////////////////////////////
 
 	# Failsafe: Never load this script in non-interactive shells, e.g. SFTP, SCP or rsync
-	[[ -t 0 && $PS1 && $- == *i* ]] || return 0
+	[[ -t 0 && $PS1 && $- == *'i'* ]] || return 0
 
 	# DietPi-Globals: dietpi-* aliases, G_* functions and variables
 	. /DietPi/dietpi/func/dietpi-globals || { echo -e '[\e[31mFAILED\e[0m] DietPi-Login | Failed to load DietPi-Globals. Skipping DietPi login scripts...'; return 1; }
@@ -26,9 +26,8 @@
 	# Workaround if SSH client overrides locale with "POSIX" fallback: https://github.com/MichaIng/DietPi/issues/1540#issuecomment-367066178
 	if [[ $(locale) =~ 'POSIX' ]]; then
 
-		current_locale=$(grep -m1 '^[[:blank:]]*AUTO_SETUP_LOCALE=' /DietPi/dietpi.txt | sed 's/^[^=]*=//')
-		export LANG=${current_locale:=en_GB.UTF-8}
-		export LC_ALL=$current_locale
+		current_locale=$(sed -n '/^[[:blank:]]*AUTO_SETUP_LOCALE=/{s/^[^=]*=//p;q}' /DietPi/dietpi.txt)
+		export LC_ALL=${current_locale:-en_GB.UTF-8}
 		unset current_locale
 
 	fi
@@ -37,12 +36,10 @@
 	if [[ $SSH_TTY ]] && ! toe -a | grep -q "^$TERM[[:blank:]]"; then
 
 		TERM_old=$TERM
-		ncurses_term=''
-
-		[[ $TERM =~ 256 ]] && export TERM='xterm-256color' || export TERM='xterm'
+		[[ $TERM == *'256'* ]] && export TERM='xterm-256color' || export TERM='xterm'
 
 		G_WHIP_MENU_ARRAY=('0' 'Ignore for now, I will change the SSH clients terminal.')
-
+		ncurses_term=
 		if ! dpkg-query -s ncurses-term &> /dev/null; then
 
 			ncurses_term=' or install the "ncurses-term" APT packages, which enables a wider terminal support'
@@ -54,7 +51,6 @@
 		G_PROGRAM_NAME='Unsupported SSH client terminal' G_WHIP_MENU "[WARNING] Your SSH client passed an unsupported terminal: TERM=$TERM_old\n
 As a workaround we fooled the server by setting: TERM=$TERM. This is not the cleanest solution, since commands might expect colours or formats, that are not supported by the actual terminal.\n
 Please change your SSH clients terminal, respectively the passed \$TERM string$ncurses_term." && (( $G_WHIP_RETURNED_VALUE )) && G_AGI ncurses-term
-
 		unset TERM_old ncurses_term
 
 	fi
