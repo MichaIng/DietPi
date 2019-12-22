8f054fbc433db83072758d623f1b46f11b2cd978
MichaIng <micha@dietpi.com>
2019-12-22 20:34:50 +0100

    v6.27
    + DietPi-PREP | Typo

--- PREP_SYSTEM_FOR_DIETPI.sh
+++ PREP_SYSTEM_FOR_DIETPI.sh
@@ -1688,7 +1688,7 @@ _EOF_
 -----------------------
 DietPi - GPLv2 License:
 -----------------------
- - Use arrow keys to scrolll
+ - Use arrow keys to scroll
  - Press 'TAB' then 'ENTER' to continue
 
 This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 2 of the License, or any later version.
