3f4cc7b0e14d275fe82b7974fce78e10a22e0eb2
MichaIng <micha@dietpi.com>
2020-06-24 20:40:25 +0200

    v6.31
    + DietPi-PREP | Remove "pi" user autologin on Raspberry Pi OS (64-bit): https://github.com/MichaIng/DietPi/issues/3570#issuecomment-648988475

--- PREP_SYSTEM_FOR_DIETPI.sh
+++ PREP_SYSTEM_FOR_DIETPI.sh
@@ -1200,8 +1200,9 @@ _EOF_
 		#	Below required if DietPi-PREP is executed from chroot/container, so RPi firstrun scripts are not executed
 		[[ -f '/etc/init.d/resize2fs_once' ]] && rm -v /etc/init.d/resize2fs_once # https://github.com/RPi-Distro/pi-gen/blob/master/stage2/01-sys-tweaks/files/resize2fs_once
 		[[ -f '/boot/cmdline.txt' ]] && sed -i 's| init=/usr/lib/raspi-config/init_resize\.sh||' /boot/cmdline.txt # https://github.com/RPi-Distro/pi-gen/blob/master/stage2/01-sys-tweaks/00-patches/07-resize-init.diff
+		[[ -f '/etc/systemd/system/getty@tty1.service.d/autologin.conf' ]] && rm -v /etc/systemd/system/getty@tty1.service.d/autologin.conf # https://github.com/MichaIng/DietPi/issues/3570#issuecomment-648988475
 
-		# - make_nas_processes_faster cron job on Rock64 + NanoPi + Pine64(?) images
+		# - make_nas_processes_faster cron job on ROCK64 + NanoPi + PINE A64(?) images
 		[[ -f '/etc/cron.d/make_nas_processes_faster' ]] && rm /etc/cron.d/make_nas_processes_faster
 
 		#-----------------------------------------------------------------------------------
