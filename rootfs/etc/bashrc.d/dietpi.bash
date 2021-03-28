f8c0b5616728ef1034ea15f2108ae6516eb2b29d
MichaIng <micha@dietpi.com>
2021-03-28 16:26:15 +0200

    v7.1
    + DietPi-Bash | Align DietPi-VPN alias with changed path

--- rootfs/etc/bashrc.d/dietpi.bash
+++ rootfs/etc/bashrc.d/dietpi.bash
@@ -48,7 +48,7 @@
 	alias dietpi-optimal_mtu='/boot/dietpi/func/dietpi-optimal_mtu'
 	alias dietpi-cloudshell='/boot/dietpi/dietpi-cloudshell'
 	alias dietpi-nordvpn='G_DIETPI-NOTIFY 1 "DietPi-NordVPN has been renamed to DietPi-VPN. Please use the \"dietpi-vpn\" command."'
-	alias dietpi-vpn="/boot/dietpi/misc/dietpi-vpn"
+	alias dietpi-vpn="/boot/dietpi/dietpi-vpn"
 	alias cpu='/boot/dietpi/dietpi-cpuinfo'
 	# - Optional DietPi software aliases
 	[[ -d '/mnt/dietpi_userdata/sonarr' || -d '/mnt/dietpi_userdata/radarr' || -d '/mnt/dietpi_userdata/lidarr' ]] && alias dietpi-arr_to_RAM='/boot/dietpi/misc/dietpi-arr_to_RAM'
