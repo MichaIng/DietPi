d6e47bebfc5ad0200de39c9ce6efd8052f887dbf
MichaIng <micha@dietpi.com>
2022-08-25 20:48:11 +0200

    v8.8
    - CI | Attempt to fix vaultwarden build for ARMv8 on GitHub runner by building it on disk instead of tmpfs

--- .build/software/vaultwarden/build.bash
+++ .build/software/vaultwarden/build.bash
@@ -23,6 +23,9 @@ G_EXEC_OUTPUT=1 G_EXEC ./rustup-init.sh -y --profile minimal --default-toolchain
 G_EXEC_NOHALT=1 G_EXEC rm rustup-init.sh
 export PATH="$HOME/.cargo/bin:$PATH"
 
+# ARMv8: Build on disk, else GitHub workflow fails due to insufficient RAM
+(( $G_HW_ARCH == 3 )) && G_EXEC cd /root
+
 version='1.25.2'
 G_DIETPI-NOTIFY 2 "Building vaultwarden version \e[33m$version"
 [[ -d vaultwarden-$version ]] && G_EXEC rm -R "vaultwarden-$version"
@@ -30,15 +33,14 @@ G_EXEC curl -sSfLO "https://github.com/dani-garcia/vaultwarden/archive/$version.
 G_EXEC tar xf "$version.tar.gz"
 G_EXEC rm "$version.tar.gz"
 G_EXEC cd "vaultwarden-$version"
-free
-df /tmp
-G_EXEC_OUTPUT=1 G_EXEC cargo build -vv --features sqlite --release
+G_EXEC_OUTPUT=1 G_EXEC cargo build --features sqlite --release
 G_EXEC rustup self uninstall -y
 G_EXEC strip --remove-section=.comment --remove-section=.note target/release/vaultwarden
 
 # Build DEB package
 G_DIETPI-NOTIFY 2 'Building vaultwarden DEB package'
 G_EXEC cd "$HOME"
+(( $G_HW_ARCH == 3 )) && G_EXEC mv "/root/vaultwarden-$version" .
 grep -q 'raspbian' /etc/os-release && DIR='vaultwarden_armv6l' || DIR="vaultwarden_$G_HW_ARCH_NAME"
 [[ -d $DIR ]] && G_EXEC rm -R "$DIR"
 G_EXEC mkdir -p "$DIR/"{DEBIAN,opt/vaultwarden,mnt/dietpi_userdata/vaultwarden,lib/systemd/system}
