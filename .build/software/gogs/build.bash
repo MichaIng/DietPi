ceb6b92a730348e6b3daa265342b46e7a474c5f9
MichaIng <micha@dietpi.com>
2023-01-21 04:05:50 +0100

    v8.14
    - CI | Gogs: Fix path to Gogs binary

--- .build/software/gogs/build.bash
+++ .build/software/gogs/build.bash
@@ -12,7 +12,7 @@ G_EXEC_OUTPUT=1 G_EXEC go install 'gogs.io/gogs@latest'
 
 # Archive
 G_EXEC mkdir gogs
-G_EXEC mv go/bin/gogs gogs/
+G_EXEC mv /root/go/bin/gogs gogs/
 G_EXEC 7zz a -mx=9 "/tmp/gogs_$G_HW_ARCH_NAME.7z" gogs
 (( $G_HW_ARCH == 1 )) && G_EXEC zip -9r /tmp/gogs_armv6.zip gogs
 
