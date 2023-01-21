bc8cc2041d73a152e2906c767ca839aa7b67aee8
MichaIng <micha@dietpi.com>
2023-01-21 01:39:36 +0100

    v8.14
    - CI | Gogs: ARMv6 builds need to be done in our container

---
+++ .build/software/gogs/build.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+{
+. /boot/dietpi/func/dietpi-globals
+
+# Build deps
+G_AGI gcc libc6-dev 7zip zip
+
+# Download & Build
+. /etc/bashrc.d/go.sh
+G_EXEC_OUTPUT=1 G_EXEC go install 'gogs.io/gogs@latest'
+
+# Archive
+G_EXEC mkdir gogs
+G_EXEC mv go/bin/gogs gogs/
+G_EXEC 7zz a -mx=9 "/tmp/gogs_$G_HW_ARCH_NAME.7z" gogs
+(( $G_HW_ARCH == 1 )) && G_EXEC zip -9r /tmp/gogs_armv6.zip gogs
+
+# Cleanup
+G_EXEC rm -R gogs
+
+exit 0
+}
