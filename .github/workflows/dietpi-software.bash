92dc2d37912d1f800fcb2e2cb531833d9a6b7e16
MichaIng <micha@dietpi.com>
2023-06-03 21:43:37 +0200

    v8.18
    - CI | DietPi-Software: Since the PiJuice service does not start without I2C device, not present in container, use a test command instead

--- .github/workflows/dietpi-software.bash
+++ .github/workflows/dietpi-software.bash
@@ -131,11 +131,12 @@ Process_Software()
 			97) aCOMMANDS[i]='openvpn --version';; # aSERVICES[i]='openvpn' aUDP[i]='1194' GitHub Action runners to not support the TUN module
 			98) aSERVICES[i]='haproxy' aTCP[i]='80';;
 			99) aSERVICES[i]='node_exporter' aTCP[i]='9100';;
-			100) aSERVICES[i]='pijuice';; # aTCP[i]='????';;
+			100) aCOMMANDS[i]='/usr/bin/pijuice_cli32 -V' # aSERVICES[i]='pijuice' aTCP[i]='????' Service does not start without I2C device, not present in container
 			104) aSERVICES[i]='dropbear' aTCP[i]='22';;
 			105) aSERVICES[i]='ssh' aTCP[i]='22';;
 			106) aSERVICES[i]='lidarr' aTCP[i]='8686';;
 			107) aSERVICES[i]='rtorrent' aTCP[i]='49164' aUDP[i]='6881';;
+			# shellcheck disable=SC2016
 			108) aCOMMANDS[i]='LD_LIBRARY_PATH=/mnt/dietpi_userdata/amiberry/lib /mnt/dietpi_userdata/amiberry/amiberry -h | grep '\''^$VER: Amiberry '\';;
 			109) aSERVICES[i]='nfs-kernel-server' aTCP[i]='2049';;
 			110) aCOMMANDS[i]='mount.nfs -V';;
