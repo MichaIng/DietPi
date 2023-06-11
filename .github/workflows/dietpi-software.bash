384f740097f98aa2196ff2539a4f85e4ff04df7f
MichaIng <micha@dietpi.com>
2023-06-12 00:47:09 +0200

    v8.19
    - CI | DietPi-Software: Fix Komga test on emulated container and add Beets and VSCodium test commands

--- .github/workflows/dietpi-software.bash
+++ .github/workflows/dietpi-software.bash
@@ -196,7 +196,7 @@ Process_Software()
 			176) aSERVICES[i]='mycroft';;
 			177) aSERVICES[i]='firefox-sync' aTCP[i]='5002';;
 			178) aSERVICES[i]='jellyfin' aTCP[i]='8097'; [[ $arch == [23] ]] && aDELAY[i]=300;; # jellyfin[9983]: arm-binfmt-P: ../../target/arm/translate.c:9659: thumb_tr_translate_insn: Assertion `(dc->base.pc_next & 1) == 0' failed.   ###   jellyfin[9983]: qemu: uncaught target signal 6 (Aborted) - core dumped   ###   about 5 times
-			179) aSERVICES[i]='komga' aTCP[i]='2037'; (( $arch == 10 )) && aDELAY[i]=30 || aDELAY[i]=90;;
+			179) aSERVICES[i]='komga' aTCP[i]='2037'; (( $arch == 10 )) && aDELAY[i]=30 || aDELAY[i]=120;;
 			180) aSERVICES[i]='bazarr' aTCP[i]='6767'; (( $arch == 10 )) && aDELAY[i]=30; (( $arch < 10 )) && aDELAY[i]=60;;
 			181) aSERVICES[i]='papermc' aTCP[i]='25565';;
 			182) aSERVICES[i]='unbound' aUDP[i]='53'; [[ ${aSERVICES[126]} ]] && aUDP[i]+=' 5335';; # Uses port 5335 if Pi-hole or AdGuard Home is installed, but those do listen on port 53 instead
@@ -206,6 +206,8 @@ Process_Software()
 			186) aSERVICES[i]='ipfs' aTCP[i]='5003 8087';;
 			187) aSERVICES[i]='cups' aTCP[i]='631';;
 			188) aCOMMANDS[i]='go version';;
+			189) aCOMMANDS[i]='codium -v';;
+			190) aCOMMADDS[i]='beet version';;
 			191) aSERVICES[i]='snapserver' aTCP[i]='1780';;
 			192) aSERVICES[i]='snapclient';;
 			194) aSERVICES[i]='postgresql';;
