45f2ca44d6a584515770447262bec582b7878500
MichaIng <micha@dietpi.com>
2023-06-16 18:25:31 +0200

    v8.19
    - DietPi-Software | Komga: Disable for RISC-V due to missing support from an embedded Java library

--- .github/workflows/dietpi-software.bash
+++ .github/workflows/dietpi-software.bash
@@ -194,7 +194,7 @@ Process_Software()
 			174) aCOMMANDS[i]='gimp -v';;
 			176) aSERVICES[i]='mycroft';;
 			178) aSERVICES[i]='jellyfin' aTCP[i]='8097'; [[ $arch == [23] ]] && aDELAY[i]=300;; # jellyfin[9983]: arm-binfmt-P: ../../target/arm/translate.c:9659: thumb_tr_translate_insn: Assertion `(dc->base.pc_next & 1) == 0' failed.   ###   jellyfin[9983]: qemu: uncaught target signal 6 (Aborted) - core dumped   ###   about 5 times
-			179) aSERVICES[i]='komga' aTCP[i]='2037'; (( $arch == 10 )) && aDELAY[i]=30 || aDELAY[i]=300; (( $arch == 11 )) && aDELAY[i]=900;;
+			179) aSERVICES[i]='komga' aTCP[i]='2037'; (( $arch == 10 )) && aDELAY[i]=30; (( $arch < 10 )) && aDELAY[i]=300;;
 			180) aSERVICES[i]='bazarr' aTCP[i]='6767'; (( $arch == 10 )) && aDELAY[i]=30; (( $arch < 10 )) && aDELAY[i]=60;;
 			181) aSERVICES[i]='papermc' aTCP[i]='25565';;
 			182) aSERVICES[i]='unbound' aUDP[i]='53'; [[ ${aSERVICES[126]} ]] && aUDP[i]+=' 5335';; # Uses port 5335 if Pi-hole or AdGuard Home is installed, but those do listen on port 53 instead
@@ -218,8 +218,8 @@ Process_Software()
 			201) aSERVICES[i]='zerotier-one' aTCP[i]='9993';;
 			202) aCOMMANDS[i]='rclone -h';;
 			203) aSERVICES[i]='readarr' aTCP[i]='8787'; [[ $arch == [23] ]] && aDELAY[i]=60;;
-			204) aSERVICES[i]='navidrome' aTCP[i]='4533';;
-			206) aSERVICES[i]='openhab' aTCP[i]='8444';;
+			204) aSERVICES[i]='navidrome' aTCP[i]='4533'; (( $arch > 9 )) || aDELAY[i]=30;;
+			206) aSERVICES[i]='openhab' aTCP[i]='8444'; [[ $arch == [23] || $arch == 11 ]] && aDELAY[i]=30;;
 			#207) Moonlight (CLI), "moonlight" command
 			#208) Moonlight (GUI), "moonlight-qt" command
 			209) aCOMMANDS[i]='restic version';;
