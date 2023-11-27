c865ee61719afb82a5d3fb7303180fa7ccaf491f
MichaIng <micha@dietpi.com>
2023-11-27 19:12:58 +0100

    v8.25 (#6782)
    - DietPi-Software | Ampache: Resolved an issue on Bullseye and Bookworm systems where the initial web UI access failed because our pre-generated database was too old. A template shipped with Ampache will now be used, the initial admin user and music catalogue added via CLI. Many thanks to `@mostly_offline` for reporting this issue: https://dietpi.com/forum/t/bypassing-ampache-update-page/17367

--- .github/workflows/dietpi-software.bash
+++ .github/workflows/dietpi-software.bash
@@ -365,12 +365,19 @@ G_EXEC eval 'echo -e '\''[Service]\nAmbientCapabilities='\'' > rootfs/etc/system
 
 # Workarounds for failing MariaDB install on Buster within GitHub Actions runner (both cannot be replicated on my test systems with and without QEMU):
 # - mysqld does not have write access if our symlink is in place, even that directory permissions are correct.
-# - Type=notify leads to a service start timeout while mysqld has actually fully started.
+# - Type=notify leads to a service start timeout while mysqld has actually fully started. However, with Type=exec, /etc/mysql/debian-start starts too soon after mysqld, so that the UNIX socket is not yet ready to listen. Hence add a second to wait for it.
 if [[ $DISTRO == 'buster' ]]
 then
 	G_EXEC sed -i '/# Start DietPi-Software/a\sed -i -e '\''s|rm -Rf /var/lib/mysql|rm -Rf /mnd/dietpi_userdata/mysql|'\'' -e '\''s|ln -s /mnt/dietpi_userdata/mysql /var/lib/mysql|ln -s /var/lib/mysql /mnt/dietpi_userdata/mysql|'\'' /boot/dietpi/dietpi-software' rootfs/boot/dietpi/dietpi-login
 	G_EXEC mkdir rootfs/etc/systemd/system/mariadb.service.d
-	G_EXEC eval 'echo -e '\''[Service]\nType=exec'\'' > rootfs/etc/systemd/system/mariadb.service.d/dietpi-container.conf'
+	cat << '_EOF_' > rootfs/etc/systemd/system/mariadb.service.d/dietpi-container.conf || exit 1
+[Service]
+Type=exec
+ExecStartPost=
+ExecStartPost=/bin/sleep 1
+ExecStartPost=/bin/sh -c "systemctl unset-environment _WSREP_START_POSITION"
+ExecStartPost=/etc/mysql/debian-start
+_EOF_
 fi
 
 # Workaround for failing 32-bit ARM Rust builds on ext4 in QEMU emulated container on 64-bit host: https://github.com/rust-lang/cargo/issues/9545
