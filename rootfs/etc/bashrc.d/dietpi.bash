f6da1f87fd0e81b33329a27082fe50688c373624
MichaIng <micha@dietpi.com>
2025-02-09 18:42:41 +0100

    dietpi-display: add new script to control console display modes

--- rootfs/etc/bashrc.d/dietpi.bash
+++ rootfs/etc/bashrc.d/dietpi.bash
@@ -50,6 +50,7 @@
 	alias dietpi-nordvpn='G_DIETPI-NOTIFY 1 "DietPi-NordVPN has been renamed to DietPi-VPN. Please use the \"dietpi-vpn\" command."'
 	alias dietpi-vpn='/boot/dietpi/dietpi-vpn'
 	alias dietpi-ddns='/boot/dietpi/dietpi-ddns'
+	alias dietpi-display='/boot/dietpi/dietpi-display'
 	alias cpu='/boot/dietpi/dietpi-cpuinfo'
 	# - Optional DietPi software aliases
 	[[ -d '/mnt/dietpi_userdata/sonarr' || -d '/mnt/dietpi_userdata/radarr' || -d '/mnt/dietpi_userdata/lidarr' ]] && alias dietpi-arr_to_RAM='/boot/dietpi/misc/dietpi-arr_to_RAM'
