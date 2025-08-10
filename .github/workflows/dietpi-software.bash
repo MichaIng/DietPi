6e51ad7766fc6fa071e33e2a16fd37f53f601bdf
MichaIng <micha@dietpi.com>
2025-08-10 13:58:59 +0200

    shellcheck: mute annotation

--- .github/workflows/dietpi-software.bash
+++ .github/workflows/dietpi-software.bash
@@ -524,7 +524,7 @@ _EOF_
 done
 
 # Success flag and shutdown
-# shellcheck disable=SC2016
+# shellcheck disable=SC2015,SC2016
 G_EXEC eval 'echo '\''[ $exit_code = 0 ] && > /success || { journalctl -n 50; ss -tulpn; df -h; free -h; }; systemctl start poweroff.target; exit $?'\'' >> rootfs/boot/Automation_Custom_Script.sh'
 
 # Shutdown as well on failures before the custom script is executed
