491285f4ad72f0621dcdba58f352c05232b892af
MichaIng <micha@dietpi.com>
2025-10-12 19:37:42 +0200

    autologin: add additional baudrate
    matching recent Debian/systemd versions

--- rootfs/var/lib/dietpi/services/dietpi-firstboot.bash
+++ rootfs/var/lib/dietpi/services/dietpi-firstboot.bash
@@ -175,7 +175,7 @@ _EOF_
 			cat << '_EOF_' > /etc/systemd/system/console-getty.service.d/dietpi-autologin.conf
 [Service]
 ExecStart=
-ExecStart=-/sbin/agetty -a root -J -s console 115200,38400,9600 $TERM
+ExecStart=-/sbin/agetty -a root -J -s console 115200,57600,38400,9600 $TERM
 _EOF_
 		fi
 
