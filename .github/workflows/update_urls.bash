cbfbc748b137498c4998f2c6b80774f2183ddfe0
MichaIng <micha@dietpi.com>
2026-02-01 13:20:03 +0100

    dietpi-software: Gogs: ARMv8 releases now have "arm64" suffix

--- .github/workflows/update_urls.bash
+++ .github/workflows/update_urls.bash
@@ -74,7 +74,7 @@ aREGEX[$software_id]='https://github.com/owncloud/ocis/releases/download/.*/ocis
 # Gogs
 software_id=49
 aCHECK[$software_id]='curl -sSf '\''https://api.github.com/repos/gogs/gogs/releases/latest'\'' | mawk -F\" "/^ *\"browser_download_url\": \".*\/gogs_[^\"\/]*_linux_$arch.tar.gz\"$/{print \$4}"'
-aARCH[$software_id]='armv8 amd64'
+aARCH[$software_id]='arm64 amd64'
 aARCH_CHECK[$software_id]='riscv64'
 aREGEX[$software_id]='https://github.com/gogs/gogs/releases/download/.*/gogs_.*_linux_\$arch.tar.gz'
 
